51d2b4928ce19a3c3d5a13876ff2cfbbe3e8252d
Saksham <saksham@noumena.global>
2025-07-28 18:05:32 +0530

    feat(entity): form

---
+++ src/entity/entityForm.ts
@@ -0,0 +1,22 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { nanoid } from 'nanoid';
+import { User } from './entityUser';
+
+@Entity('forms')
+@Unique(['token'])
+export class Form {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column({ default: nanoid() })
+  token!: string;
+
+  @CreateDateColumn({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt!: Date;
+
+  @ManyToOne(() => User, user => user.token)
+  user!: User;
+}
