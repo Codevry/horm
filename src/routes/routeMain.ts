027f8c65ae6c0a0a7a95c05c8555ca6253b59d63
Saksham <saksham@noumena.global>
2025-07-29 11:16:00 +0530

    feat(route): main - adding form

--- src/routes/routeMain.ts
+++ src/routes/routeMain.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import { DbInit } from '@/database/dbInit.ts';
 import Globals from '@/utils/globals.ts';
 import { routeUser } from '@/routes/routeUser.ts';
+import routeForm from '@/routes/routeForm.ts';
 
 export default class RouteMain {
   private readonly app: Hono;
@@ -29,6 +30,7 @@ export default class RouteMain {
     Globals.dataSource = await DbInit.initialize();
     this.defaultRoutes();
     this.app.route('/users', routeUser);
+    this.app.route('/forms', routeForm);
     return this.app;
   }
 }
