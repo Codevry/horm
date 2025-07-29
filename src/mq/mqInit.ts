34c94c15148fb6d9e0257afbc291afb78d67a770
Saksham <saksham@noumena.global>
2025-07-29 14:36:21 +0530

    feat(mq): init

---
+++ src/mq/mqInit.ts
@@ -0,0 +1,11 @@
+import IORedis from 'ioredis';
+import Globals from '@/utils/globals.ts';
+import MessageQueue from '@/mq/queue.ts';
+import WorkerRecordNew from '@/mq/workerRecordNew.ts';
+import { ENUM_QUEUE } from '@/utils/enums.ts';
+
+const connection = new IORedis({ db: 1 });
+
+// --- new record queue
+Globals.mqQueueRecord = new MessageQueue(connection, ENUM_QUEUE.newRecord);
+WorkerRecordNew(ENUM_QUEUE.newRecord);
