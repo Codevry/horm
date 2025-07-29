ef627f6d98f9a8c7e9a9ff341ac17970075b883a
Saksham <saksham@noumena.global>
2025-07-29 11:34:54 +0530

    feat(route): new - set userToken

--- src/routes/routeForm.ts
+++ src/routes/routeForm.ts
@@ -6,6 +6,8 @@ const route = new Hono();
 
 route.post('/', middlewareAuthAdmin, async c => {
   const body = await c.req.json();
+  body.userToken = c.get('userToken');
+
   const result = await Globals.ctrlForm.create(body);
   return c.json(result);
 });
