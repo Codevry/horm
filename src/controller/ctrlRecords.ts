c5887b8fe822956ef9a7d538f0bfa044e2e9491e
Saksham <saksham@noumena.global>
2025-07-29 13:16:38 +0530

    feat(ctrl): record - get all records for given form & fix issue with create

--- src/controller/ctrlRecords.ts
+++ src/controller/ctrlRecords.ts
@@ -14,8 +14,11 @@ export default class CtrlRecords {
       // check if name exists
       if (!data || !token) throw new Error('data & formToken are required');
 
-      // check if the form exists
-      await Globals.ctrlForm.getByToken({ formToken: token });
+      // check if the form exists & is active
+      const form = await Globals.ctrlForm.getByToken({ formToken: token });
+      if (form.error) return form;
+      else if (form?.data?.isActive === false)
+        throw new Error('Form is disabled. Please enable the form to create a record');
 
       // create a new form
       const recordsRepository = Globals.dataSource.getRepository(Records);
@@ -38,6 +41,51 @@ export default class CtrlRecords {
       return {
         status: false,
         message: 'Failed to create record',
+        error: error.message || '',
+      };
+    }
+  }
+
+  /**
+   * get all records for a form
+   */
+  async getByForm(body: any) {
+    try {
+      // get form token from the body
+      const token = body.formToken;
+
+      // check if token exists
+      if (!token) throw new Error('formToken is required');
+
+      // check if the form exists
+      const form = await Globals.ctrlForm.getByToken({ formToken: token });
+      if (form.error) return form;
+
+      // get a record repository
+      const recordsRepository = Globals.dataSource.getRepository(Records);
+
+      // find all records for the form sorted by createdAt
+      const records = await recordsRepository.find({
+        where: {
+          form: {
+            token,
+          },
+        },
+        order: {
+          createdAt: 'DESC',
+        },
+      });
+
+      // return response
+      return {
+        status: true,
+        message: 'Records retrieved successfully',
+        data: records,
+      };
+    } catch (error: Error | any) {
+      return {
+        status: false,
+        message: 'Failed to retrieve records',
         error: error.message || '',
       };
     }
