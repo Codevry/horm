7bb120ce9f9d4c14bbbfaa0523b216610b97e830
Saksham <saksham@noumena.global>
2025-07-29 11:17:50 +0530

    feat(route): form - create new

---
+++ src/routes/routeForm.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono';
+import middlewareAuthAdmin from '@/middlewares/middlewareAuthAdmin.ts';
+import Globals from '@/utils/globals.ts';
+
+const route = new Hono();
+
+route.post('/', middlewareAuthAdmin, async c => {
+  const body = await c.req.json();
+  const result = await Globals.ctrlForm.create(body);
+  return c.json(result);
+});
+
+export default route;
