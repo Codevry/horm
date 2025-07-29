7c09751d00151361c7515dc4582f318db5566337
Saksham <saksham@noumena.global>
2025-07-29 13:23:12 +0530

    feat(route) - records - set cors on create

--- src/routes/routeRecord.ts
+++ src/routes/routeRecord.ts
@@ -1,15 +1,26 @@
 import { Hono } from 'hono';
 import Globals from '@/utils/globals.ts';
 import middlewareAuthAdmin from '@/middlewares/middlewareAuthAdmin.ts';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
+const corsOrigin = Bun.env.CORS?.split(',') || ['*'];
 
 // create new record
-app.post('/', async c => {
-  const body = await c.req.json();
-  const result = await Globals.ctrlRecords.create(body);
-  return c.json(result);
-});
+app.post(
+  '/',
+  cors({
+    origin: corsOrigin,
+    allowMethods: ['POST', 'OPTIONS'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+  }),
+  async c => {
+    const body = await c.req.json();
+    const result = await Globals.ctrlRecords.create(body);
+    return c.json(result);
+  },
+);
 
 // get all records for a form
 app.get('/form/:formId', middlewareAuthAdmin, async c => {
