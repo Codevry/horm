891a1510ffc3c1d12d6ba79eadd7f103b28cca87
Saksham <saksham@noumena.global>
2025-07-29 14:37:15 +0530

    feat: bun - smtp vars

--- src/bun.ts
+++ src/bun.ts
@@ -2,5 +2,12 @@ declare module 'bun' {
   interface Env {
     PORT: number;
     CORS: string;
+    SMTP_HOST: string;
+    SMTP_PORT: number;
+    SMTP_USER: string;
+    SMTP_PASS: string;
+    SMTP_FROM: string;
+    SMTP_SECURE: boolean;
+    RECORD_EMAIL_TO: string;
   }
 }
