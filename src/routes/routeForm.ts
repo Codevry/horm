9f14e471df1ec98ac63937afdfc0eb850d2c2f03
Saksham <saksham@noumena.global>
2025-07-29 11:39:19 +0530

    feat(route): form - get

--- src/routes/routeForm.ts
+++ src/routes/routeForm.ts
@@ -4,11 +4,18 @@ import Globals from '@/utils/globals.ts';
 
 const route = new Hono();
 
+// create a new form
 route.post('/', middlewareAuthAdmin, async c => {
   const body = await c.req.json();
   body.userToken = c.get('userToken');
 
   const result = await Globals.ctrlForm.create(body);
+  return c.json(result);
+});
+
+// get all forms (sorted by createdAt)
+route.get('/', middlewareAuthAdmin, async c => {
+  const result = await Globals.ctrlForm.get();
   return c.json(result);
 });
 
