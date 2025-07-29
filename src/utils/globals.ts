7ea09e4be72db335db05e4edbe3970bba7ba60ce
Saksham <saksham@noumena.global>
2025-07-29 14:17:09 +0530

    feat(globals): queueRecord

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,9 +1,11 @@
 import type { DataSource } from 'typeorm';
 import CtrlForm from '@/controller/ctrlForm.ts';
 import CtrlRecords from '@/controller/ctrlRecords.ts';
+import MessageQueue from '@/mq/queue.ts';
 
 export default class Globals {
   static dataSource: DataSource;
   static ctrlForm = new CtrlForm();
   static ctrlRecords = new CtrlRecords();
+  static mqQueueRecord: MessageQueue;
 }
