16b2c386f1ae78cf13a65d6384da2c23772cec66
Saksham <saksham@noumena.global>
2025-07-29 11:17:06 +0530

    feat(globals): ctrlForm

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,5 +1,7 @@
 import type { DataSource } from 'typeorm';
+import CtrlForm from '@/controller/ctrlForm.ts';
 
 export default class Globals {
   static dataSource: DataSource;
+  static ctrlForm = new CtrlForm();
 }
